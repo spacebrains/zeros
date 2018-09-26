67d0da54fde1454e5eaa1975c3df3fecb80e50df
sergei yezhgunovich <f.glomgold@yandex.ru>
2018-09-26 17:36:58 +0300

    zeros of factorial

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  let result=0;
+  let factor=5
+  while(number/factor>=1){
+      result+=Math.floor(number/factor);
+      factor*=5;
+  }
+  return result
 }
+
